2edd2b97babf79ff573e7dc381ff688b790f5551
Ukasha0306 <ukasha0306@gmail.com>
2024-06-23 11:10:32 +0500

    first commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -9,94 +9,107 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    final provider = Provider.of<ViewProvider>(context);
+
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text('Quiz Score : ${provider.quizScore}')),
-              SizedBox(
-                height: size.height * 0.05,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Consumer<ViewProvider>(
+          builder: (context, value, child) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Question no : ${provider.questionNo}',
-                    style: const TextStyle(fontWeight: FontWeight.w600),
+                  SizedBox(
+                    height: size.height * 0.03,
                   ),
-                  Text('Time Left : ${provider.seconds} seconds'),
-                ],
-              ),
-              SizedBox(
-                height: size.height * 0.05,
-              ),
-              Text(
-                SampleData.quizQuestionList[provider.currentIndex].question
-                    .toString(),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: size.height * 0.05,
-              ),
-              Column(
-                children: List.generate(
-                  SampleData.quizQuestionList[provider.currentIndex].options.length,
-                  (index) {
-                    final option = SampleData.quizQuestionList[provider.currentIndex].options[index];
-                    final isCorrect = index == SampleData.quizQuestionList[provider.currentIndex].answerIndex;
-                    return TQuizOptionTiles(
-                      option: option,
-                      isCorrect: isCorrect,
-                      isSelected: index == provider.selectedIndex,
-                      onTap: () {
-                         provider.updateSelectedIndex(index);
-                        if (!isCorrect) {
-                          provider.updateSelectedIndex(SampleData.quizQuestionList[provider.currentIndex].answerIndex);
+                  Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text('Quiz Score : ${value.quizScore}')),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Question no : ${value.questionNo}',
+                        style: const TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      Text('Time Left : ${value.seconds} seconds'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Text(
+                    SampleData.quizQuestionList[value.currentIndex].question
+                        .toString(),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Column(
+                    children: List.generate(
+                      SampleData
+                          .quizQuestionList[value.currentIndex].options.length,
+                      (index) {
+                        final option = SampleData
+                            .quizQuestionList[value.currentIndex]
+                            .options[index];
+                        final isCorrect = index ==
+                            SampleData.quizQuestionList[value.currentIndex]
+                                .answerIndex;
+                        return TQuizOptionTiles(
+                          option: option,
+                          isCorrect: isCorrect,
+                          isSelected: index == value.selectedIndex,
+                          onTap: () {
+                            value.updateSelectedIndex(index);
+                            if (!isCorrect) {
+                              value.updateSelectedIndex(SampleData
+                                  .quizQuestionList[value.currentIndex]
+                                  .answerIndex);
+                            } else {
+                              value.updateQuizScore();
+                            }
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: blackColor,
+                        foregroundColor: whiteColor,
+                      ),
+                      onPressed: () {
+                        if (value.currentIndex ==
+                            SampleData.quizQuestionList.length - 1) {
+                          value.showEndDialog(context);
+                          value.timer!.cancel();
                         } else {
-                          provider.updateQuizScore();
+                          value.nextQuestion(context);
                         }
                       },
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                height: size.height * 0.05,
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: blackColor,
-                    foregroundColor: whiteColor,
+                      child: value.currentIndex ==
+                              SampleData.quizQuestionList.length - 1
+                          ? const Text('Submit')
+                          : const Text('Next'),
+                    ),
                   ),
-                  onPressed: () {
-                    if (provider.currentIndex == SampleData.quizQuestionList.length - 1) {
-                      provider.showEndDialog(context);
-                      provider.timer!.cancel();
-                    } else {
-                      provider.nextQuestion(context);
-                    }
-                  },
-                  child:provider.currentIndex == SampleData.quizQuestionList.length-1 ? const Text('Submit') : const Text('Next'),
-                ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
